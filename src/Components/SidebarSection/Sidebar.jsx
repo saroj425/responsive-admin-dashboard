fb945b21c60e011de57df04e53bfea7414a4a277
Sarojmilkanoagro <saroj@milkanoagro.com>
2023-11-27 05:58:04 +0530

    new push

---
+++ src/Components/SidebarSection/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar grid'>
+      <div className='logoDiv flex'>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
